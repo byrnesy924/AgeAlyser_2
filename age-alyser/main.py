42de7d7004ba8a5e56dc60b2ec8c69ff8da3ca88
byrnesy924 <b.byrnesy@gmail.com>
2024-08-15 23:42:29 +1000

    added class to model production buildings

--- age-alyser/main.py
+++ age-alyser/main.py
@@ -885,6 +885,34 @@ class AgeGame:
         return
 
 
+class ProductionBuilding:
+    """This class models the function of a production building, including creating units, storing upgrades, measuring idle time"""
+    def __init__(self, 
+                 building_type: str, 
+                 civilisation: str, 
+                 unit_queue_times: pd.DataFrame, 
+                 relevant_technologytimes: pd.DataFrame,
+                 building_id: int = None  # Data may limit this being able to be used
+                 ) -> None:
+        # Parse information
+
+        # TODO add error handling that all civilisations are valid, all units passed are valid, the type of building is valid
+
+        pass
+
+    def produce_units(self) -> pd.DataFrame:
+        """Take the time stamps of units, as well as the upgrades, and work out when they wouldve been produced, 
+        taking into account 1 at a time creation (i.e., queuing)
+
+        :return pd.DataFrame: a dataframe of units and when they were created
+
+        """
+        pass
+
+    def count_building_idle_time(self) -> int:
+        pass
+
+
 if __name__ == "__main__":
     # TODO think about the best structure of the module in order to create a good API and workflow 
     # - what information is must-have, what is optional
