cc2633eeb44bfb34b0ce54f3bd5b8b912a19828a
byrnesy924 <b.byrnesy@gmail.com>
2024-10-28 22:02:51 +1100

    starting regression tests

---
+++ tests/Test_Games/README_notes.txt
@@ -0,0 +1,2 @@
+Test replay #1:
+Viper uses more stables than they create! this used to cause bugs but just need to investigate now. Player 2.
---
+++ tests/test.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+# import pytest
+
+from agealyser.main import AgeGame
+
+
+def regression_testing():
+    results = []
+    # get the suite of test games
+    test_files = [game for game in os.listdir(r"tests\\Test_Games") if ".aoe2record" in game]  # TODO fix pathing when automating
+    test_games = [AgeGame(r"tests\\Test_Games\\" + file) for file in test_files]
+
+    # run the package on these
+    results = pd.concat([game.advanced_parser(include_map_analyses=True) for game in test_games])
+
+    # load the correct map results
+    # correct_results = pd.read_csv("regression_testing_correct_results.csv")
+
+    results.to_csv(r"Test_Games\\regression_testing_correct_results.csv")
+
+    assert pd.testing.assert_frame_equal(results, correct_results)
+
+if __name__=="__main__":
+    regression_testing()
