a3f9db411f14b5fb8e2a0ee289b73e8701f60d57
byrnesy924 <b.byrnesy@gmail.com>
2025-02-03 13:58:18 +0100

    updating tests

--- tests/test.py
+++ tests/test.py
@@ -14,18 +14,18 @@ def regression_testing():
     # run the package on these
     results = pd.concat([game.advanced_parser(include_map_analyses=True) for game in test_games], axis=1)
 
-    for player in test_games[0].players:
-        player.inputs_df.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_inputs.csv")
-        player.actions_df.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_actions.csv")
-        temp = pd.Series(player.technologies)
-        temp.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_technologies.csv")
-        player.buildings.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_buildings.csv")
-        player.queue_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_queue.csv")
-        player.unqueue_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_unqueue.csv")
-        player.military_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_units.csv")
+    # for player in test_games[0].players:
+    #     player.inputs_df.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_inputs.csv")
+    #     player.actions_df.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_actions.csv")
+    #     temp = pd.Series(player.technologies)
+    #     temp.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_technologies.csv")
+    #     player.buildings.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_buildings.csv")
+    #     player.queue_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_queue.csv")
+    #     player.unqueue_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_unqueue.csv")
+    #     player.military_units.to_csv(fr"tests\\Test_Games\\Game_Data_Structures\\Player{player.number}_units.csv")
 
     # load the correct map results
-    correct_results = pd.read_csv(r"tests\\Test_Games\\regression_testing_correct_results.csv")
+    correct_results = pd.read_csv(r"tests\\Test_Games\\regression_testing_correct_results.csv", index_col=0)
 
     results.to_csv(r"tests\\Test_Games\\regression_testing_test_results.csv")
 
